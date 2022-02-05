3ed8a63113fc453795d84e2aad9d9ab94894b366
0xjsthon <torikussadik404@gmail.com>
2022-02-05 10:17:39 +0600

    condition

---
+++ Basics/all.py
@@ -0,0 +1,65 @@
+
+# =-= if statement =-=
+
+
+# saarc = ['bangladesh','afganisthan','bhutan','nepal','india','pakisthan','sri lanka'];
+# country = input('Enter a country name: ')
+# if country in saarc:
+#     print(country, ' is a member of saarc');
+
+# print('program termiateed')
+
+
+
+# saarc = ['bangladesh','afganisthan','bhutan','nepal','india','pakisthan','sri lanka'];
+# country = input('Enter a country name: ')
+# if country in saarc:
+#     print(country, ' is a member of saarc');
+# else:
+#     print(country, ' is not a memebr of saarc')
+
+# print('program termiateed')
+
+
+
+#  checking grade 
+
+# marks = input('Enter a mark here:');
+# marks = int(marks)
+
+# if marks >= 80:
+#     print('A+')
+# elif marks >= 70:
+#     print('A');
+# elif marks >= 60:
+#     print('A-')
+# elif marks >= 50:
+#     print('B')
+# else:
+#     print('F')
+
+
+
+# marks = input('Enter a mark here');
+# marks = int(marks)
+
+# if marks >= 80:
+#     grade = 'A+'
+# elif marks >= 70:
+#     grade = 'A'
+# elif marks >= 80:
+#     grade = 'A-'
+# elif marks >= 70:
+#     grade = 'A'
+# elif marks >= 60:
+#     grade = 'A-'
+# elif marks >= 40:
+#     grade = 'F'
+# print('your grade is ', grade)
+
+
+
+
+
+
+
