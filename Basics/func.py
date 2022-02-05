da0453ea1a125310ea94d145715bebdda76d52a3
0xjsthon <torikussadik404@gmail.com>
2022-02-05 22:41:50 +0600

    function

---
+++ Basics/func.py
@@ -0,0 +1,52 @@
+# ------------= Python function =--------------
+
+
+# def add(num1, num2):
+#     return num1 + num2
+
+# ans = add(10,20)
+# print(ans)
+
+
+# draw square 
+
+# import turtle
+# turtle.shape('turtle')
+# turtle.color('blue')
+# turtle.speed(4)
+
+
+# def draw_square(side_length):
+#     for i in range(4):
+#         turtle.forward(side_length)
+#         turtle.left(90)
+
+# draw_square(200)
+# turtle.exitonclick()
+
+
+
+
+
+
+
+
+
+# import turtle
+# turtle.shape('turtle')
+# turtle.color('green')
+# turtle.speed(7)
+
+# def draw_square(side_length):
+#     for i in range(4):
+#         turtle.forward(side_length)
+#         turtle.right(90)
+
+# counter = 0
+# while counter < 90:
+#     draw_square(100)
+#     turtle.right(4)
+#     counter +=1
+
+# turtle.exitonclick()
+
