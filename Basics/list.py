fef4ab1990c37ea5bf7c5e7976f13a6dc7c1042e
0xjsthon <torikussadik404@gmail.com>
2022-02-08 00:40:39 +0600

    list

---
+++ Basics/list.py
@@ -0,0 +1,57 @@
+
+
+
+
+# -> append() 
+
+# name = ['torikus','sadik','fahim','shakil']
+# name.append('raihan')
+# print(name)
+
+
+# -> sort()
+
+# name = ['torikus','sadik','fahim','shakil']
+# name.sort()
+# print(name)
+
+
+# -> reverse()
+
+# name = ['torikus','sadik','fahim','shakil']
+# name.sort()
+# name.reverse()
+# print(name)
+
+
+# -> insert()
+
+# name = ['torikus','sadik','fahim','shakil']
+# name.insert(0, 'tori')
+# print(name)
+
+
+# -> remove()
+
+# name = ['torikus','sadik','fahim','shakil']
+# name.remove('torikus')
+# print(name)
+
+
+name = ['torikus','sadik','fahim','shakil']
+item = 'tori'
+if item in name:
+    item.remove()
+else:
+    print('Sorry! buddy, This item is not in this list')
+print(name)
+
+
+
+
+
+
+
+
+
+
