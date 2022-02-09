4f96a626c1427efcb9bb50d789b848ccc5e529d3
0xjsthon <torikussadik404@gmail.com>
2022-02-09 19:26:52 +0600

    loop practice

--- Basics/loop.py
+++ Basics/loop.py
@@ -32,5 +32,103 @@
 #     print(i)
 
 
+# -> else
+
+# i = 1
+# while i < 6:
+#     print(i)
+#     i += 1
+# else:
+#     print('i is no longer less than 6')
+
+
+
+
+
+
+
+# --------------> for loop <-------------------
+
+
+# fruits = ['apple','banana','cherry']
+# for x in fruits:
+#     print(x)
+
+
+
+# loop throught the letters in word 
+
+
+# for x in "torikus sadik":
+#     print(x)
+
+
+# fruits = ['banana','apple','orange']
+# for x in fruits:
+#     print(x)
+#     if x == "banana":
+#         break
+
+
+
+
+
+# fruits = ['banana','apple','orange']
+# for x in fruits:
+#     if x == 'banana':
+#         break
+#     print(x)
+
+
+# fruits = ['kiwi','banana','apple','orange']
+# for x in fruits:
+#     if x == 'banana':
+#         continue
+#     print(x)
+
+
+
+# for x in range(6):
+#     print(x)
+
+
+# for x in range(2, 6):
+#     print(x)
+
+
+# for x in range(2, 30, 3):
+#     print(x)
+
+
+
+# for x in range(6):
+#     print(x)
+# else:
+#     print('Finally finished!')
+
+
+
+
+# for x in range(6):
+#     if x == 3: break
+#     print(x)
+# else:
+#     print("Finally finished!")
+
+
+
+# -> Nested loop 
+
+# adj = ['red','big','tasty']
+# fruits = ['apple','banana','cherry']
+
+# for x in adj:
+#     for y in fruits:
+#         print(x, y)
+
+
+# for x in ['shakil','fahim','torikus']:
+#     pass
+
 
 
