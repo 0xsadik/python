7938dd5c6242233f77eea2a4fcce6884d0e7c5b6
0xjsthon <torikussadik404@gmail.com>
2022-02-11 15:53:49 +0600

    boring stuff

--- Basics/atbswp.py
+++ Basics/atbswp.py
@@ -36,10 +36,101 @@
 # print(sToNum)
 # print(sToNum + 2)
 
+# --- float to integer 
+
+# fl = 12.22
+# integer = int(fl)
+# print(integer)
+
 
 
 
 # -> float()
+
+# int to float 
+
+# real = 10
+# floating = float(real)
+# print(floating)
+
+
+
+
+
+
+
+"""
+
+    =====================================
+    ============ Flow control ===========
+    ===================================== 
+
+"""
+
+
+# pass check 
+
+# name = input('please enter your name: ')
+# password = input('Please enter your password here:')
+
+# if name == 'Shakil' or name == 'shakil':
+#     print('hello, ', name)
+# if password == 'shakkhi':
+#     print('welcome to your world!', name)
+# else:
+#     print('Correct your password dude!')
+
+
+
+# elif 
+
+# name = input('name: ')
+# age = int(input('age: '))
+
+# if name == 'shakil':
+#     print('hey,', name)
+# elif age < 12:
+#     print('You are not shakil, kiddo')
+# elif age > 2000:
+#     print('you are alien')
+# elif age > 100:
+#     print('hi dado you are not shakil')
+
+
+
+
+
+
+# --> while loop :
+
+# spam = 0
+# while spam < 5:
+#     print('aaaaaa a a a aa ')
+#     spam += 1
+
+
+# name = ''
+# while name != 'torikus':
+#     print('Please type your name:')
+#     name = input()
+# print('thank you')
+
+
+
+
+# while True:
+#     print('please type your name: ')
+#     name = input()
+#     if name == 'torikus':
+#         break
+# print('Thank you!')
+
+
+
+
+
+
+
 
 
 
