534217994de4298de965c554fc8aff032f587e05
0xjsthon <torikussadik404@gmail.com>
2022-02-11 15:32:24 +0600

    boring stuff

--- Basics/atbswp.py
+++ Basics/atbswp.py
@@ -14,6 +14,39 @@
 
 
 
+# -> len()
+
+# name = 'torikus sadik'
+# count = len(name)
+# print(count)
+
+
+# -> str()
+
+# number = 2022
+# number_to_string = str(number)
+# print(number_to_string)
+
+
+
+# -> int()
+
+# snum = '2022'
+# sToNum = int(snum)
+# print(sToNum)
+# print(sToNum + 2)
+
+
+
+
+# -> float()
+
+
+
+
+
+
+
 
 
 
