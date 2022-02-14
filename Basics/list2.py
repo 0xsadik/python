1b184dc5327abfbc6d25b45e5b8e8f3c15e730de
0xjsthon <torikussadik404@gmail.com>
2022-02-14 22:57:23 +0600

    list

---
+++ Basics/list2.py
@@ -0,0 +1,50 @@
+
+# automate boring list 
+
+
+# frnds = ['shakil','babu','fahim','morshed','torikus','sadik']
+# # fmt = frnds[2:5]
+# startToEnd = frnds[0:4]
+# print(startToEnd)
+
+
+# removing values from lists with del statements
+
+# spam = ['shakil','babu','fahim','morshed','torikus','sadik']
+# del spam[5]
+# print(spam)
+
+
+# -> working with lists 
+
+# print('Enter the name of cat 1: ')
+# catname1 = input()
+# print('Enter the name of cat 2: ')
+# catname2 = input()
+# print('Enter the name of cat 3: ')
+# catname3 = input()
+# print('Enter the name of cat 4: ')
+# catname4 = input()
+# print('Enter the name of cat 5: ')
+# catname5 = input()
+
+# print(catname1 + ' ' + catname2 + ' ' + catname3 + ' ' + catname4 + ' ' + catname5)
+
+
+
+
+# catNames = []
+# while True:
+#     print('Enter the name of cat ' + str(len(catNames) + 1) + ' (or enter nothing to stop.): ')
+#     name = input()
+#     if name == '':
+#         break
+#     catNames = catNames + [name] 
+# print('The cat names are:')
+# for name in catNames:
+#     print('  ' + name)
+
+
+
+
+
