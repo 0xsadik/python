917710237044a1aed9094e32382d4502744e2e19
0xjsthon <torikussadik404@gmail.com>
2022-02-16 19:20:33 +0600

    set

---
+++ Basics/set.py
@@ -0,0 +1,14 @@
+"""
+    =========== Sets ============
+"""
+
+
+# set are used to store multiple items in a sing le variable 
+
+# a set is a collection which is unordered, unchangeable and unindexed
+
+
+# set
+
+tset = {'apple','banana','cherry'}
+print(tset)
