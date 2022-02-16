b3873f2d1494200962fd919aca46a92f30063043
0xjsthon <torikussadik404@gmail.com>
2022-02-16 19:18:45 +0600

    tuple

--- Basics/tuple.py
+++ Basics/tuple.py
@@ -43,10 +43,77 @@
 
 
 
+# using Asterisk (*)
+
+# fruits = ('apple','banana','cherry','strawberry','raspberry')
+# (green, yellow, *red) = fruits
+
+# print(green)
+# print(yellow)
+# print(red)
+
+
+# fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+# (green, *tropic, red) = fruits
+# print(green)
+# print(tropic)
+# print(red)
 
 
 
+# ======> python loop tuples <=========
 
+
+
+# tt = ('apple','banana','cherry')
+# for x in tt:
+#     print(x)
+
+
+# loop through the index numbers 
+
+# tt = ('apple','banana','cherry')
+# for i in range(len(tt)):
+#     print(tt[i])
+
+
+
+# using a while loop
+
+# tt = ('apple','banana','cherry')
+# i = 0
+# while i < len(tt):
+#     print(tt[i])
+#     i = i + 1
+
+
+# multiply tuples 
+
+# fruits = ('apple','banana','pineapple')
+# mytuple = fruits * 2
+# print(mytuple)
+
+
+
+# python tuple methods -> count()
+#                      -> index()
+
+
+
+# count()
+
+# tt = (1, 3, 7, 8, 5, 4, 6, 8, 5, 6, 5)
+# x = tt.count(5)
+# print(x)
+
+
+# index()
+
+
+# tt = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+# tt = (1, 3, 7, 5, 4, 8, 7, 5, 4, 6, 8, 5)
+# a = tt.index(8)
+# print(a)
 
 
 
