c90b366881d900326d0334fbda9532487318bf38
0xjsthon <torikussadik404@gmail.com>
2022-02-16 19:00:17 +0600

    tuple

---
+++ Basics/tuple.py
@@ -0,0 +1,59 @@
+# tuple 
+
+# texample = ('apple','bananan','pineapple')
+# print(texample)
+
+
+# check type 
+
+# te = ('ghori',)
+# print(te)
+# print(type(te))
+
+
+# ex = ('appple','banana','pineapple','kiwi')
+# zero = ex[0]
+# print(zero)
+
+# rst = ex[:2]
+# print(rst)
+
+
+
+#convert the tuple into a list and add anotehr item/element
+
+# ex = ('appple','banana','pineapple','kiwi')
+# y = list(ex)
+# y.append('watermelon')
+# print(y)
+# ex = tuple(y)
+# print(ex)
+
+
+
+# ======> unpack tuples <=======
+
+
+
+# fruits = ('apple','banana','pineapple')
+# (green, yellow, red) = fruits
+# print(green)
+# print(yellow)
+# print(red)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
