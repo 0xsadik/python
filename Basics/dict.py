0db30dfdb7cb482a01012a61068c8f4b1c50b5b4
0xjsthon <torikussadik404@gmail.com>
2022-02-18 14:57:45 +0600

    dictionary

---
+++ Basics/dict.py
@@ -0,0 +1,70 @@
+# ================= python dictionaries =======================
+
+
+# Dictionary
+
+# dictionary = {
+#     "name" : "torikus",
+#     "age" : 24,
+#     "country" : "Bangladesh",
+#     "year" : 2022
+# }
+# print(dictionary)
+
+
+
+# Dictionary items 
+
+# tdic = {
+#     'brand' : 'Ford',
+#     'model' : 'mustang',
+#     'year' : 1723
+# }
+# print(tdic['year'])
+
+
+# duplicate not allowed
+
+# tdic = {
+#     'brand' : 'Ford',
+#     'model' : 'mustang',
+#     'year' : 2022,
+# }
+# print(tdic)
+
+
+# dictionary items  - Data types 
+
+
+# tdic = {
+#     'brand' : 'Ford',
+#     'electric' : False,
+#     'year' : 1964,
+#     'colors' : ['red','white','blue']
+# }
+# print(tdic)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
