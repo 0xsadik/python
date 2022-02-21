762edd1bdd142c90f24209c6d585472f35c2c484
0xjsthon <torikussadik404@gmail.com>
2022-02-21 12:45:24 +0600

    practice

--- Basics/practice.py
+++ Basics/practice.py
@@ -248,7 +248,56 @@
 
 
 
+# random number game 
 
+# import random 
+
+# number = random.randint(1, 1000)
+# attempts = 0
+
+# while True:
+#     input_number = input('Guess the number (between 1 to 100)')
+#     input_number = int(input_number)
+
+#     attempts += 1
+#     if input_number == number :
+#         print('yes your guess is correct! ')
+#         break
+#     if input_number > number :
+#         print('Incorrect! pelase try to guess  a smaller number.')
+#     else:
+#         print('Incorrect! please try to guess a larger number. ')
+
+# print('You tried!', attempts, 'times to find the correct number.')
+
+
+
+
+
+# import random 
+
+# number = random.randint(1, 1000)
+# attempts = 0
+# low = 1
+# high = 1000
+
+# while True:
+#     print('Guess the correct number (between 1 to 1000):')
+#     input_number = (low + high) // 2
+#     print('My guess is ', input_number)
+#     attempts += 1
+
+#     if input_number == number:
+#         print('Yes! your guess is correct.')
+#         break
+#     if input_number > number:
+#         print('Incorrect! please try to guess a smaller number. ')
+#         high = input_number - 1
+#     else:
+#         print('Incorrect! please try to guess a larger number.')
+#         low = input_number + 1
+
+# print(f'You tried: {attempts} times to find the correct number')
 
 
 
