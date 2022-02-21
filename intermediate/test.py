21c45c9c25013e9b6dd983e736a4041d4bba1ad6
0xjsthon <torikussadik404@gmail.com>
2022-02-21 19:53:29 +0600

    up

---
+++ intermediate/test.py
@@ -0,0 +1,30 @@
+
+
+# import math
+
+# a = math.pi
+# print(a)
+
+# print(math.pow(2, 3))
+# print(math.pow(3, 2))
+
+# print(math.sqrt(25))
+# print(math.ceil(5.3))
+
+# ------------
+
+
+# import datetime 
+
+# today = datetime.date.today()
+# print(today)
+
+
+
+# import datetime 
+
+# today = datetime.datetime.today()
+# print(today)
+
+
+
