37f6ba344918afefd8adb8b63809f3e0af4b4020
0xjsthon <torikussadik404@gmail.com>
2022-02-21 20:07:47 +0600

    obj and class

--- intermediate/objandclass.py
+++ intermediate/objandclass.py
@@ -1,2 +1,66 @@
 # object and class 
 
+
+# -> example - 0
+
+
+# class Car:
+#     name = 'premio'
+#     color = 'white'
+
+#     def start():
+#         print('starting engine....')
+
+# print('Name of the car is:', Car.name)
+# print('Color of the car is :', Car.color)
+# Car.start()
+
+
+
+
+# -> example - 1
+
+# class Car :
+#     name = ''
+#     color = ''
+    
+#     def start():
+#         print('staring engine ...')
+    
+# Car.name = 'Axio'
+# Car.color = 'Red'
+# print('Name of the car is: ', Car.name)
+# print('Color of the car is : ', Car.color)
+# Car.start()
+
+
+
+# -> example - 2
+
+
+# class car:
+#     name = ''
+#     color = ''
+
+#     def start(self):
+#         print('car is satarting...')
+    
+# my_car = car()
+# my_car.name = 'Alison'
+# print(my_car.name)
+# my_car.start()
+
+
+
+# self a little bit 
+
+# class check:
+#     def __init__(self):
+#         print('Adress of self = ', id(self))
+
+# obj = check()
+# print('Address of class object = ', id(obj))
+
+
+
+
