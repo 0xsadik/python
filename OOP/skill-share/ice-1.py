be2656084f9997107a033bb29db509a2b1585085
0xjsthon <torikussadik404@gmail.com>
2022-02-24 14:59:10 +0600

    ice- 1

--- OOP/skill-share/ice-1.py
+++ OOP/skill-share/ice-1.py
@@ -49,17 +49,78 @@
 
 
 
-class IceCream:
-    def __init__(self):
-        self.scoops = 3
+# class IceCream:
+#     def __init__(self):
+#         self.scoops = 3
 
-    def eat(self, scoops):
-        self.scoops -= scoops
+#     def eat(self, scoops):
+#         if self.scoops < scoops:
+#             print('Not enough bits left!')
+#         else:
+#             self.scoops -= scoops
     
-    def add(self, scoops):
-        self.scoops += scoops 
+#     def add(self, scoops):
+#         self.scoops += scoops 
     
+# cream = IceCream()
+# cream.eat(5)
+
+
+
+# -> Creating lights 
+
+
+# -> class 
+# -> Method 
+# -> Dot Expression
+# -> constructor 
+# -> Attribute 
+# -> Practice 
+
+
+# class Light:
+#     def __init__(self):
+#         self.on = False
     
+#     def toggle(self):
+#         self.on = not self.on
+
+# light = Light()
+# light.toggle()
+# print(light.on)
+
+
+
+# => 
+
+# class Light:
+#     def __init__(self):
+#         self.on = False
+    
+#     def is_on(self):
+#         return self.on
+    
+#     def toggle(self):
+#         self.on = not self.on
+    
+# light = Light()
+# print(light.is_on())
+# light.toggle()
+# print(light.is_on())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
