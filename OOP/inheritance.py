25f4813106ff7731d25cd201d5a11b3e0e60880a
0xjsthon <torikussadik404@gmail.com>
2022-02-25 01:38:58 +0600

    inheritance

--- OOP/inheritance.py
+++ OOP/inheritance.py
@@ -47,6 +47,107 @@
 # car.change_gear('D')
 
 
+
+
+
+# class Vehicle:
+#     """ Base class for all vehicles """
+
+#     def __init__(self, name, manufacturer, color):
+#         self.name = name
+#         self.manufacturer = manufacturer
+#         self.color = color 
+
+#     def drive(self):
+#         print(f'Driving {self.manufacturer} {self.name}')
+    
+#     def turn(self, direction):
+#         print(f'Turning {self.name} to {direction}')
+    
+#     def brake(self):
+#         print(f'{self.name} is stopping')
+
+
+# class Car(Vehicle):
+#     """ Car class """
+
+#     def change_gear(self, gear_name):
+#         """ Method for changing gear """
+#         print(f'{self.name} is changing gear to {gear_name}')
+
+
+# if __name__ == "__main__":
+#     c =  Car('Mustang 5.0 GT coupe', 'Ford','Red')
+#     c.drive()
+#     c.turn('right')
+#     c.brake()
+#     c.change_gear('P')
+
+
+
+
+# -> Creating constructor 
+
+
+# class Vehicle:
+#     """ Base clas for all vehicles """
+
+#     def __init__(self, name, manufracturer, color):
+#         self.name = name
+#         self.manufacturer - manufracturer
+#         self.color = color 
+
+#     def drive(self):
+#         print(f'Driving {self.manufacturer} {self.name}')
+    
+#     def turn(self, direction):
+#         print(f'Turning {self.name} to {direction}')
+    
+#     def brake(self):
+#         print(f'{self.name} is stopping')
+
+# class Car(Vehicle):
+#     """ Car class """
+
+#     def __init__(self, name, manufracturer, color, year):
+#         self.name = name
+#         self.manufracturer = manufracturer
+#         self.color = color 
+#         self.year = 2017
+#         self.wheels = 4
+#         print(f'A new car has been created. Name {self.name}')
+#         print(f'It has {self.wheels} wheels')
+#         print(f'The car was built in {self.year}')
+
+#     def change_gear(self, gear_name):
+#         """ Method of chaning gear"""
+#         print('{self.name} is changing to {gear_name}')
+
+    
+# if __name__ == "__main__":
+#     c = Car('Mustang 5.0 GT coupe ', 'Ford','Red', 2017)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # -> inheritance anis sir
 
 # class Phone:
