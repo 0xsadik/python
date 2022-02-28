cbc1ef53f31061bfd04c397d58c46b25dc78eb9f
0xjsthon <torikussadik404@gmail.com>
2022-03-01 00:06:36 +0600

    up

---
+++ OOP/skill-share/deluxeIceCreamTruck.py
@@ -0,0 +1,16 @@
+
+
+
+# --> Inheritance 
+
+
+# class Light:
+
+#     def __init__(self, sync = None):
+#         super().__init__()
+#         self.on = False
+#         self.sync = sync
+
+#     def is_on(self):
+
+
--- OOP/skill-share/ice-1.py
+++ OOP/skill-share/ice-1.py
@@ -193,33 +193,8 @@
 
 
 
-class Light:
 
-  def __init__(self, sync=None): # new - tuple
-    super().__init__()
-    self.on = False
-    self.sync = sync
 
-  def is_on(self):
-    return self.on
-
-  def toggle(self):
-    self.on = not self.on
-    if self.sync is not None:
-      self.sync.toggle()
-
-class OldLight(Light):
-
-  def __init__(self, sync=None):
-    super().__init__(sync=sync)
-    self.on = False
-    self.sync = sync
-    self.flicker = False
-
-  def toggle(self):
-    super().toggle()
-    if self.on:
-      self.flicker = not self.flicker
 
 
 
