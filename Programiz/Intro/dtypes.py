21c660973db552bfa4fb986c812a11e9a942601f
0xjsthon <torikussadik404@gmail.com>
2022-03-21 23:41:38 +0600

    programiz

---
+++ Programiz/Intro/dtypes.py
@@ -0,0 +1,64 @@
+
+# -> python numbers
+
+# a = 5
+# print(a, 'is of type:', type(a))
+
+# a = 2.0
+# print(f'{a} is of type: {type(a)}')
+
+# a = 1 + 2j
+# print(f'{a} is complex number? {isinstance(1 + 2j, complex)}')
+
+
+
+# -> python list
+ 
+
+# a = [1, 2.2, 'python']
+
+
+# a = [5, 110, 20, 30, 290, 233, 111, 234, 23]
+# print('a[2] = ', a[2])
+
+# print('a[0:3] =', a[0:3])
+
+# print('a[5:] =', a[5:])
+
+
+
+
+# -> python Tuple 
+
+
+# t = (5, 'program', 1 + 3j)
+# print(t)
+
+
+
+# t = (5,'program', 1+3j)
+
+# print('t[1] =', t[1])
+
+# print('t[0:3] = ', t[0:3])
+
+# tuple are immutable 
+
+
+
+# -> Python set 
+
+
+# a = {5, 2, 3, 1, 4}
+
+# print('a = ', a)
+# print(type(a))
+
+
+
+
+
+
+
+
+
---
+++ Programiz/Intro/namespace.py
@@ -0,0 +1,18 @@
+# python namespace 
+
+
+# you may get different values for the id 
+
+# a = 2 
+# print('id 2 =', id(2))
+# print('id(a) =', id(a))
+
+
+# a = 2 
+# print('id(a) = ', id(a))
+
+# a = a + 1 
+# print('id(a) =', id(a))
+# print('id(3) =', id(3))
+
+
