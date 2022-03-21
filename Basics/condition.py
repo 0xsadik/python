f0c19c3353cb1be576b9d78fb8e59cf43e954681
0xjsthon <torikussadik404@gmail.com>
2022-03-22 00:16:39 +0600

    condition

---
+++ Basics/condition.py
@@ -0,0 +1,87 @@
+
+
+# --> Python conditions and If statements 
+
+
+
+
+# ------> if 
+
+
+# a = 33
+# b = 200
+# if b > a:
+#     print('b is greater than a ')
+
+
+
+# ---> elif 
+
+# a = 33
+# b = 33
+# if b > a:
+#     print('b is greater than a')
+# elif a == b :
+#     print('a and b are equal ')
+
+
+
+
+# ---> else 
+
+# a = 300
+# b = 22
+
+# if b > a :
+#     print('b is greater than a')
+
+# elif a == b:
+#     print('a and b are equal')
+
+# else:
+#     print('a is greater than b ')
+
+
+
+
+# ---> Short hand if 
+
+# a = 33
+# b = 22
+# if a > b : print('a is greater than b')
+
+
+# ---> short hand if ... else 
+
+# a = 2 
+# b = 323
+
+# print('a') if a > b else print('b')
+
+
+
+# ---> and 
+
+# a = 200
+# b = 12
+# c = 304
+
+# if a > b and c > a :
+#     print('both condition are True')
+
+
+
+
+# ---> or 
+
+# a = 200
+# b = 33
+# c = 400
+# if a > b or a > c:
+#     print('At least one of the conditions is True ')
+
+
+
+
+
+
